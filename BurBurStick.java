d31e6a5ab4fbaacf4785b26e29cbe589ef1c6a63
LiIndexUwU <93841328+LiIndexUwU@users.noreply.github.com>
2021-11-14 14:00:22 +0100

    Create BurBurStick.java

---
+++ BurBurStick.java
@@ -0,0 +1,16 @@
+package BurBurStick;
+
+public class BurBurStick {
+  class uploader {
+    int width = 30;
+    int height = 20;
+    int text = "upload file";
+  }
+  if (this.uploader.uploadedFile()) {
+    String cracx = this.crackFile();
+    if (cracx) {
+      this.downloadFile(this.cracx.getFile());
+      System.out.printIn("cracked succesfully");
+    }
+  }
+}
